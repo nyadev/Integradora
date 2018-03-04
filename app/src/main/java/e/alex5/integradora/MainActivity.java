6d9b5fa5099ec5e4d2ebe49423f20982a3d6fae4
nyadev <nyadev.ipn@gmail.com>
2018-03-04 11:00:52 -0600

    Primer commit

---
+++ app/src/main/java/e/alex5/integradora/MainActivity.java
@@ -0,0 +1,13 @@
+package e.alex5.integradora;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
